7a651110c3e46f332d94c3348e5b16b668e1d6a3
ZainJutt931 <58330847+ZainJutt931@users.noreply.github.com>
2019-11-29 22:07:57 +0500

    Add files via upload

---
+++ 3.17.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class ElectricBill 
+{
+public:
+   ElectricBill( double ap ) {
+      setAmountPaid( ap );
+   }
+   void setAmountPaid( double ap ) {
+      amountPaid = ap;
+      if ( ap < 0 )
+         amountPaid = 0;
+   }
+   double getAmountPaid(){
+      return amountPaid;
+   }
+   double getVAT(){
+      return amountPaid * 0.18;
+   }
+   double getEWURA() {
+      return amountPaid * 0.01;
+   }
+   double getREA(){
+      return amountPaid * 0.03;
+   }
+   double getUnits(){
+      return ( amountPaid - getVAT() - getREA() - getEWURA() - 5000 ) / 295;
+   }
+private:
+   double amountPaid;
+};
+int main() {
+   double amount;
+   cout << "Amount (Tanzanian Shillings): ";
+   cin >> amount;
+   ElectricBill electricBill( amount );
+   cout << "\nAmount paid:    " << electricBill.getAmountPaid() << " TZS"
+        << "\nUnits provided: " << electricBill.getUnits() << " kWh"
+        << "\nVAT (18%):      " << electricBill.getVAT() << " TZS"
+        << "\nREA (3%):       " << electricBill.getREA() << " TZS"
+        << "\nEWURA (1%):     " << electricBill.getEWURA() << " TZS"
+        << "\nFixed charge:   5000 TZS" << endl;
+   return 0;
+}
